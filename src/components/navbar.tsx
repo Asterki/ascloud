b40d7a0767d58c87d019cb16393c96996c723b5c
Asterki <asterki.dev@proton.me>
2023-06-23 18:04:05 -0600

    Fix: moved the navbar to the component file

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -15,13 +15,66 @@ const Navbar: NextPage<ComponentProps> = (props) => {
 	return (
 		<div className={styles["component"]}>
 			<div className={styles["navbar"]}>
-				<div className={styles["branding"]}>
-					<h1>AsCloud</h1>
-					<p>Open Source Cloud/Local Storage Service</p>
+				<div className={styles["top-navbar"]}>
+					<div className={styles["branding"]}>
+						<h1>AsCloud</h1>
+						<p>Open Source Cloud/Local Storage Service</p>
+					</div>
+
+					<div
+						className={styles["navbar-menu-trigger"]}
+						onClick={() => setNavbarMenuOpen(!navbarMenuOpen)}
+					>
+						<p>{props.user.username}</p>
+					</div>
+
+					<motion.div
+						variants={{
+							open: {
+								opacity: 1,
+								transition: { duration: 0.1 },
+								position: "absolute",
+								display: "block",
+							},
+							closed: {
+								opacity: 0,
+								transition: { duration: 0.1 },
+								transitionEnd: {
+									display: "none",
+								},
+							},
+						}}
+						animate={navbarMenuOpen ? "open" : "closed"}
+						initial={"closed"}
+						className={styles["navbar-menu"]}
+					>
+						<button>ejqwoiej</button>
+						<button>ejqwoiej</button>
+						<button>ejqwoiej</button>
+						<button>ejqwoiej</button>
+					</motion.div>
 				</div>
 
-				<div className={styles["navbar-menu-trigger"]}>
-					<p>{props.user.username}</p>
+				<div className={styles["left-navbar"]}>
+					<div>
+						<button>
+							<img src="/svg/folder.svg" alt="" />
+							Personal folder
+						</button>
+						<button>
+							<img src="/svg/link.svg" alt="" />
+							Shared
+						</button>
+					</div>
+					<div>
+						<button>
+							<img src="/svg/delete-bin.svg" alt="" />
+							Trash
+						</button>
+						<br />
+						<br />
+						AsCloud, open source at <a href="">GitHub</a>
+					</div>
 				</div>
 			</div>
 		</div>
--- src/pages/main/home.tsx
+++ src/pages/main/home.tsx
@@ -221,69 +221,7 @@ const Home: NextPage<PageProps> = (props) => {
 			</Head>
 
 			<main>
-				<div className={styles["navbar"]}>
-					<div className={styles["top-navbar"]}>
-						<div className={styles["branding"]}>
-							<h1>AsCloud</h1>
-							<p>Open Source Cloud/Local Storage Service</p>
-						</div>
-
-						<div
-							className={styles["navbar-menu-trigger"]}
-							onClick={() => setNavbarMenuOpen(!isNavbarMenuOpen)}
-						>
-							<p>{props.user.username}</p>
-						</div>
-
-						<motion.div
-							variants={{
-								open: {
-									opacity: 1,
-									transition: { duration: 0.1 },
-									position: "absolute",
-									display: "block",
-								},
-								closed: {
-									opacity: 0,
-									transition: { duration: 0.1 },
-									transitionEnd: {
-										display: "none",
-									},
-								},
-							}}
-							animate={isNavbarMenuOpen ? "open" : "closed"}
-							initial={"closed"}
-							className={styles["navbar-menu"]}
-						>
-							<button>ejqwoiej</button>
-							<button>ejqwoiej</button>
-							<button>ejqwoiej</button>
-							<button>ejqwoiej</button>
-						</motion.div>
-					</div>
-
-					<div className={styles["left-navbar"]}>
-						<div>
-							<button>
-								<img src="/svg/folder.svg" alt="" />
-								Personal folder
-							</button>
-							<button>
-								<img src="/svg/link.svg" alt="" />
-								Shared
-							</button>
-						</div>
-						<div>
-							<button>
-								<img src="/svg/delete-bin.svg" alt="" />
-								Trash
-							</button>
-							<br />
-							<br />
-							AsCloud, open source at <a href="">GitHub</a>
-						</div>
-					</div>
-				</div>
+				<Navbar user={props.user} />
 
 				<div className={styles["current-folder"]}>
 					<div className={styles["folder-navbar"]}>
